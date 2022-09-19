106e7e7942552ad419d7f83665ee91c21eb78399
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-09-19 10:43:00 -0700

    Exporting script hashes to remove that dependency from app (fedorov)

--- app.go
+++ app.go
@@ -7,6 +7,13 @@ import (
 	"io"
 )
 
+var ScriptHashes = []string{
+	//script-iframe-size-receive-message.gohtml
+	"'sha256-EoiesIg5jhsIaHn7PSaZ/oT9Yi0MCUx9WzALOyH9HkE='",
+	//script-iframe-post-message.gohtml
+	"'sha256-vEdzDTUjeRFG21L/pW+qldt1k+gnTSWl4v2E16iqJPc='",
+}
+
 type PropertyLinkFormatter view_models.Formatter
 
 type ReduxApp struct {
