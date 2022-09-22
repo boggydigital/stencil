ae9dafabdd9ceb9d6e8c37c7fb10dbeddf7db485
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-09-22 11:43:22 -0700

    Exporting icons

---
+++ icons.go
@@ -0,0 +1,12 @@
+package stencil
+
+const (
+	IconWindows   = "windows"
+	IconMacOS     = "macos"
+	IconLinux     = "linux"
+	IconStar      = "star"
+	IconSparkle   = "sparkle"
+	IconStack     = "stack"
+	IconSearch    = "search"
+	IconBackToTop = "back-to-top"
+)
