b70e0e8e74047703c63397f9a83354b75eb40322
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2023-01-01 20:58:15 -0800

    Bringing back image loading animation

--- script_hashes.go
+++ script_hashes.go
@@ -1,10 +1,12 @@
 package stencil
 
 var ScriptHashes = []string{
-	//script-iframe-size-receive-message.gohtml
+	//script_iframe_size_receive_message.gohtml
 	"'sha256-EoiesIg5jhsIaHn7PSaZ/oT9Yi0MCUx9WzALOyH9HkE='",
-	//script-iframe-post-message.gohtml
+	//script_iframe_post_message.gohtml
 	"'sha256-vEdzDTUjeRFG21L/pW+qldt1k+gnTSWl4v2E16iqJPc='",
 	//script_onfocus_digest.gohtml
 	"'sha256-n0s8aUnGgeVGVMRpMuAJ+LIRrdURaa//IaGKuDUk/WI='",
+	//script_image_loading.gohtml
+	"'sha256-Aj9d2Nl0niMrZy1w3VLlqviK5LNVKzscc4jrIbJO0B4='",
 }
