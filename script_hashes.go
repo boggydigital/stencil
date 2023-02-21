37f6307af7952731108ffc08ce318d7f885c40c7
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2023-02-21 12:44:13 -0800

    Implementing dehydrated images support for item cover images
    Deprecating placeholder image elements and using img instead
    
    Deprecating image loading script

--- script_hashes.go
+++ script_hashes.go
@@ -7,6 +7,4 @@ var ScriptHashes = []string{
 	"'sha256-vEdzDTUjeRFG21L/pW+qldt1k+gnTSWl4v2E16iqJPc='",
 	//script_onfocus_digest.gohtml
 	"'sha256-n0s8aUnGgeVGVMRpMuAJ+LIRrdURaa//IaGKuDUk/WI='",
-	//script_image_loading.gohtml
-	"'sha256-Aj9d2Nl0niMrZy1w3VLlqviK5LNVKzscc4jrIbJO0B4='",
 }
