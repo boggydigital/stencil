831ab23f89db0983fc834e63318ee82d38edeb8e
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-08-07 14:43:26 -0700

    Implementing support for the new digest endpoint that returns a map of value titles

--- script_hashes.go
+++ script_hashes.go
@@ -6,7 +6,7 @@ var ScriptHashes = []string{
 	//script_iframe_post_message.gohtml
 	"'sha256-vEdzDTUjeRFG21L/pW+qldt1k+gnTSWl4v2E16iqJPc='",
 	//script_onfocus_digest.gohtml
-	"'sha256-n0s8aUnGgeVGVMRpMuAJ+LIRrdURaa//IaGKuDUk/WI='",
+	"'sha256-xeLce5w7s50rpoIyrH5lVuwM/0tU1RtZDJJD1J4b2aA='",
 	//script_image_fadein
 	"'sha256-vnU7CtjHeJV3j7wkpQWB05X3hLelxNSIakS6ShF3Kuo='",
 }
